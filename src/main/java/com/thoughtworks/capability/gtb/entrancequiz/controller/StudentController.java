7be56c8902b8b8d6d3f124b4c5c9b66d07c69d18
imdreamer <743295483@qq.com>
2020-10-13 14:39:44 +0800

    [yangqian] feat: get all students controller and test.

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
@@ -1,7 +1,26 @@
 package com.thoughtworks.capability.gtb.entrancequiz.controller;
 
+import com.thoughtworks.capability.gtb.entrancequiz.dto.Student;
+import com.thoughtworks.capability.gtb.entrancequiz.dto.StudentResponse;
+import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("/students")
+    @ResponseBody
+    public ResponseEntity<StudentResponse<List<Student>>> getAllStudents() {
+        StudentResponse<List<Student>> allProducts = studentService.getAllStudents();
+        return ResponseEntity.ok(allProducts);
+    }
 }
--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/controllerTests/StudentControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/controllerTests/StudentControllerTest.java
@@ -1,9 +1,19 @@
 package com.thoughtworks.capability.gtb.entrancequiz.controllerTests;
 
+import com.thoughtworks.capability.gtb.entrancequiz.dto.Student;
+import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -11,4 +21,27 @@ public class StudentControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private StudentService studentService;
+
+    private Student student;
+
+    @BeforeEach
+    void setUp() {
+        StudentService.deleteAllStudents();
+        student = Student.builder()
+                .studentName("杨乾")
+                .build();
+    }
+
+    @Test
+    void should_return_all_students_when_get_all_students_success() throws Exception {
+        studentService.createStudent(student);
+        mockMvc.perform(get("/students"))
+                .andExpect(jsonPath("$.data", hasSize(1)))
+                .andExpect(jsonPath("$.data[0].studentName", is("杨乾")))
+                .andExpect(status().isOk());
+    }
+
 }
