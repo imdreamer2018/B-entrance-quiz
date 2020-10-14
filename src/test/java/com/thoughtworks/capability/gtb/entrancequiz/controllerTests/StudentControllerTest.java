0e4af0f4a9a6d2eedd6651643c5bb26de9a5fbeb
imdreamer <743295483@qq.com>
2020-10-14 23:13:54 +0800

    [yangqian] fix: student controller test.

--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/controllerTests/StudentControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/controllerTests/StudentControllerTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb.entrancequiz.controllerTests;
 
+import com.thoughtworks.capability.gtb.entrancequiz.Repository.StudentRepository;
 import com.thoughtworks.capability.gtb.entrancequiz.dto.Student;
 import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,13 +30,13 @@ public class StudentControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private StudentService studentService;
+    private StudentRepository studentRepository;
 
     private Student student;
 
     @BeforeEach
     void setUp() {
-        StudentService.deleteAllStudents();
+        studentRepository.deleteAll();
         student = Student.builder()
                 .name("杨乾")
                 .build();
@@ -43,22 +44,21 @@ public class StudentControllerTest {
 
     @Test
     void should_return_all_students_when_get_all_students_success() throws Exception {
-        StudentService.addStudent(student);
+        studentRepository.save(student);
         mockMvc.perform(get("/students"))
-                .andExpect(jsonPath("$.data", hasSize(1)))
-                .andExpect(jsonPath("$.data[0].studentName", is("杨乾")))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is("杨乾")))
                 .andExpect(status().isOk());
     }
 
     @Test
     void should_return_student_info_when_create_student_success() throws Exception {
         String jsonValue =
-                "{\"studentName\":\"杨乾\"}";
+                "{\"name\":\"杨乾\"}";
         mockMvc.perform(post("/students")
                 .content(jsonValue).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
-        List<Student> all = StudentService.getAll();
-        assertNotNull(all);
+        List<Student> all = studentRepository.findAll();
         assertEquals(all.size(), 1);
         assertEquals(all.get(0).getName(), "杨乾");
     }
