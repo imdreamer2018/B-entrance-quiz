dbe27fc3cfaaa31d671f64ece79302043651816c
imdreamer <743295483@qq.com>
2020-10-14 22:17:38 +0800

    [yangqian] fix: RequestMapping.

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
@@ -10,6 +10,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/")
 // GTB: 推荐使用@RestController，这样就可以省略@ResponseBody
 public class StudentController {
 
@@ -23,23 +24,23 @@ public class StudentController {
 
     // GTB: 可以使用类级别的@RequestMapping抽取公共的路径前缀
     // GTB: 违反Restful实践，请求成功后直接返回数据即可，不需要用StudentResponse再包一层
-    @GetMapping("/students")
+    @GetMapping("students")
     public ResponseEntity<List<Student>> getAllStudents() {
         return ResponseEntity.ok(studentService.getAllStudents());
     }
 
-    @PostMapping("/students")
+    @PostMapping("students")
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         return ResponseEntity.created(URI.create("/students")).body(studentService.createStudent(student));
     }
 
-    @GetMapping("/students/group")
+    @GetMapping("students/group")
     // GTB: 应该创建专门的对象来表示小组
     public ResponseEntity<List<Group>> getGroupStudents() {
         return ResponseEntity.ok(studentService.getGroupingStudents());
     }
 
-    @PostMapping("/students/group")
+    @PostMapping("students/group")
     public ResponseEntity<List<Group>>  groupStudents() {
         return ResponseEntity.ok(studentService.groupingStudents());
     }
